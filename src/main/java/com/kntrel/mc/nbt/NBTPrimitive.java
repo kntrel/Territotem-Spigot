f7029b7b21eafefaa74fec01954a7329c99b316b
kntrel <me@kntrel.com>
2026-02-23 19:23:14 -0600

    Completed NBTPrimitive API

--- src/main/java/com/kntrel/mc/nbt/NBTPrimitive.java
+++ src/main/java/com/kntrel/mc/nbt/NBTPrimitive.java
@@ -5,14 +5,28 @@ import com.saicone.rtag.tag.TagBase;
 public class NBTPrimitive extends NBTTag {
 
     //FIELDS
-    private final byte typeID_;
+    private final NBTType<?> type_;
 
 
+    //CONSTRUCTORS
     public NBTPrimitive(Object tag) {
         super(tag);
         if (!TagBase.isTag(tag)) {
             throw new ClassCastException("Passed object is not an NBTList");
         }
-        this.typeID_ = TagBase.getTypeId(tag);
+        this.type_ = NBTType.ofTypeId(TagBase.getTypeId(tag));
+        if (this.type_ == null) {
+            throw new IllegalArgumentException("Provided inner tag is not of a valid type id");
+        }
+    }
+
+
+    //API
+    public boolean is(NBTType<?> type) {
+        return this.type_.equals(type);
+    }
+    public <T> T get(NBTType<T> type) {
+        if (!this.is(type)) { return null; }
+        return type.type().cast(TagBase.getValue(this.handle()));
     }
 }
---
+++ src/main/java/com/kntrel/mc/nbt/NBTType.java
@@ -0,0 +1,75 @@
+package com.kntrel.mc.nbt;
+
+import com.kntrel.util.Constants;
+import org.jspecify.annotations.Nullable;
+import java.util.Arrays;
+
+public final class NBTType<T> {
+
+    //FACTORY
+    public static @Nullable NBTType<?> ofTypeId(byte typeId) {
+        for (NBTType<?> type : VALUES_) {
+            if (type.typeId_ == typeId) {
+                return type;
+            }
+        }
+        return null;
+    }
+    @SuppressWarnings("unchecked")
+    public static <T> @Nullable NBTType<T> ofType(Class<T> clazz) {
+        for (NBTType<?> type : VALUES_) {
+            if (type.type_.equals(clazz)) {
+                return (NBTType<T>) type;
+            }
+        }
+        return null;
+    }
+
+
+    //CONSTANTS
+    //Byte = 1 | Short = 2 | Int = 3 | Long = 4 | Float = 5 | Double = 6 | ByteArray = 7 | String = 8 | List = 9 | Compound = 10 | IntArray = 11 | LongArray = 12
+    public static final NBTType<Byte> BYTE = new NBTType<>(1, Byte.class);
+    public static final NBTType<Short> SHORT = new NBTType<>(2, Short.class);
+    public static final NBTType<Integer> INTEGER = new NBTType<>(3, Integer.class);
+    public static final NBTType<Long> LONG = new NBTType<>(4, Long.class);
+    public static final NBTType<Float> FLOAT = new NBTType<>(5, Float.class);
+    public static final NBTType<Double> DOUBLE = new NBTType<>(6, Double.class);
+    public static final NBTType<Byte[]> BYTE_ARRAY = new NBTType<>(7, Byte[].class);
+    public static final NBTType<String> STRING = new NBTType<>(8, String.class);
+    public static final NBTType<Integer[]> INTEGER_ARRAY = new NBTType<>(11, Integer[].class);
+    public static final NBTType<Long[]> LONG_ARRAY = new NBTType<>(12, Long[].class);
+
+    private static final NBTType<?>[] VALUES_ = Constants.getAll(NBTType.class, NBTType.class).toArray(new NBTType<?>[0]);
+    public static NBTType<?>[] values() {
+        return Arrays.copyOf(VALUES_, VALUES_.length);
+    }
+
+
+    //FIELDS
+    private final byte typeId_;
+    private final Class<T> type_;
+
+
+    //CONSTRUCTOR
+    private NBTType(int typeId, Class<T> type) {
+        this.typeId_ = (byte) typeId;
+        this.type_ = type;
+    }
+
+
+    //GETTERS
+    public byte typeId() { return this.typeId_; }
+    public Class<T> type() { return this.type_; }
+
+
+    //IMPLEMENTATION
+    @Override public boolean equals(Object o) {
+        if (o == null) { return false; }
+        if (o == this) { return true; }
+        if (!(o instanceof NBTType<?> other)) { return false; }
+        return this.typeId_ == other.typeId_;
+    }
+    @Override public int hashCode() {
+        return Byte.hashCode(this.typeId_);
+    }
+}
