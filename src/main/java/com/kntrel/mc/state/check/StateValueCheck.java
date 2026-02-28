5d3705cd06f5aed29de38a116fcab730e1959a62
kntrel <me@kntrel.com>
2026-02-28 16:33:46 -0600

    Added check factory method to StateValueCheck

--- src/main/java/com/kntrel/mc/state/check/StateValueCheck.java
+++ src/main/java/com/kntrel/mc/state/check/StateValueCheck.java
@@ -2,7 +2,20 @@ package com.kntrel.mc.state.check;
 
 import com.kntrel.mc.state.State;
 
+import java.util.function.Predicate;
+
 public abstract class StateValueCheck implements StateCheck {
+
+    //FACTORY
+    public StateValueCheck check(String valueKey, Predicate<State.Value> check) {
+        return new StateValueCheck(valueKey) {
+            @Override
+            public boolean test(State.Value value) {
+                return check.test(value);
+            }
+        };
+    }
+
 
     //FIELDS
     private String key_;
