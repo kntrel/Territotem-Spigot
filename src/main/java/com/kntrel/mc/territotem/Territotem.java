b88b6163acbfd93d63dce6e1636139647032fc66
kntrel <me@kntrel.com>
2026-03-07 22:59:06 -0600

    Rearanged and naming and code structure

--- src/main/java/com/kntrel/mc/territotem/Territotem.java
+++ src/main/java/com/kntrel/mc/territotem/Territotem.java
@@ -42,7 +42,7 @@ public final class Territotem extends JavaPlugin {
         this.chunkPersister_ = new ChunkPersister(this);
         StructureService structureService = new StructureService(this, this.chunkPersister_);
         TotemCoreTracker totemCoreTracker = new TotemCoreTracker(regionContext, structureService, this.chunkPersister_);
-        new TotemService(this);
+        new TotemService(regionContext);
         structureService.registerBlueprint(createTotemBlueprint(totemCoreTracker, hierarchy))
                 .on(TotemCoreCompletedEvent.class)
                 .when(e -> {
